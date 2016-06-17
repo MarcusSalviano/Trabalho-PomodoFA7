4ba312f5f9be267844b2389d3c682fd6773d3977
Marcus Fábio Salviano <marcussalviano@gmail.com>
2016-06-16 22:51:50 -0300

    Criando coluna isConcluido na tabela pomodoro

--- app/src/main/assets/db/create.sql
+++ app/src/main/assets/db/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE pomodoro (_id INTEGER NOT NULL PRIMARY KEY, titulo TEXT NOT NULL, descricao TEXT NOT NULL, num_pomodoros INTEGER NOT NULL);
+CREATE TABLE pomodoro (_id INTEGER NOT NULL PRIMARY KEY, titulo TEXT NOT NULL, descricao TEXT NOT NULL, num_pomodoros INTEGER NOT NULL, is_concluido INTEGER NOT NULL);
